c400a873f05f8ce2ef5a367d4139f1bc0d7cec21
qualia-5w4 <geniusnaru@gmail.com>
2023-11-06 20:13:53 +0900

    add Client class

--- src/Client.php
+++ src/Client.php
@@ -1,1 +1,11 @@
 <?php
+
+namespace gptfree;
+
+use GuzzleHttp\Client as GuzzleClient;
+
+class Client extends GuzzleClient {
+    public function __construct(array $config = []) {
+        $gptfConfig = new Config($config);
+    }
+}
